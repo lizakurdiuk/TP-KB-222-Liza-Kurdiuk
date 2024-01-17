f1b2d0696e3968486230aa66bccc27ae9dfbae16
Єлизавета Курдюк <lizakurdiuk>
2024-01-17 02:21:58 +0200

    add report file

---
+++ topic_07/task01.py
@@ -0,0 +1,14 @@
+class MyClass:
+    def __init__(self, attribute1, attribute2):
+        self.attribute1 = attribute1
+        self.attribute2 = attribute2
+
+    def some_method(self):
+        # Ваш код для методу
+        pass  # Це може бути пустий оператор, якщо метод покищо порожній
+
+# Створення об'єкта класу
+my_object = MyClass("значення1", "значення2")
+
+# Виклик методу об'єкта
+my_object.some_method()
